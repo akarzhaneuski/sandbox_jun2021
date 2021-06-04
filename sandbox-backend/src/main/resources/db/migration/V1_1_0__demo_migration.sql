e1ca75513972e5f202094e84cdb2cb48407ed396
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-04 15:02:44 +0300

    modify DB according mentors review

--- sandbox-backend/src/main/resources/db/migration/V1_1_0__demo_migration.sql
+++ sandbox-backend/src/main/resources/db/migration/V1_1_0__demo_migration.sql
@@ -6,62 +6,100 @@ CREATE TABLE IF NOT EXISTS `DEMO_TABLE`
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = UTF8;
-CREATE TABLE employee(
-                         id INT NOT NULL AUTO_INCREMENT,
-                         login VARCHAR(50) NOT NULL,
-                         password VARCHAR(255) NOT NULL,
-                         role enum ('user', 'moderator') NOT NULL,
-                         location VARCHAR(100),
-                         KEY  (id),
-                         primary key (id));
-
-CREATE TABLE company(
-                        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(50) NOT NULL,
-                        location VARCHAR(255) NOT NULL
+CREATE TABLE employee
+(
+    id          INT                        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    login       VARCHAR(50)                NOT NULL,
+    password    VARCHAR(255)               NOT NULL,
+    role        enum ('user', 'moderator') NOT NULL,
+    location_id INT,
+    modified    DATETIME,
+    modifiedBy  VARCHAR(50),
+    CONSTRAINT FK_employee_location
+        FOREIGN KEY (location_id) REFERENCES location (id),
+    KEY (id)
 );
 
-CREATE TABLE location(
-                         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                         city VARCHAR(50)
+CREATE TABLE company
+(
+    id         INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50) NOT NULL,
+    modified   DATETIME,
+    modifiedBy VARCHAR(50)
 );
 
-CREATE TABLE company_location (
-                                  company_id INT NOT NULL,
-                                  FOREIGN KEY (company_id)
-                                      REFERENCES company (id),
-                                  location_id INT NOT NULL,
-                                  FOREIGN KEY (location_id)
-                                      REFERENCES location (id)
+CREATE TABLE location
+(
+    id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    city       VARCHAR(100),
+    modified   DATETIME,
+    modifiedBy VARCHAR(50)
 );
 
-CREATE TABLE discount(
-                         id INT NOT NULL AUTO_INCREMENT,
-                         name VARCHAR(50) NOT NULL,
-                         description VARCHAR(255) NOT NULL,
-                         tag VARCHAR(50) NOT NULL,
-                         time DATETIME NOT NULL,
-                         quantity INT DEFAULT 0,
-                         promocode VARCHAR(10) NOT NULL,
-                         company_id INT,
-                         primary key (id),
-                         foreign key (company_id) references company(id));
-
-CREATE TABLE employee_discount(
-                                  employee_id INT NOT NULL,
-                                  foreign key (employee_id) references employee(id),
-                                  discount_id INT NOT NULL,
-                                  foreign key (discount_id) references discount(id)
+CREATE TABLE company_location
+(
+    id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    company_id  INT NOT NULL,
+    location_id INT NOT NULL,
+    FOREIGN KEY (company_id)
+        REFERENCES company (id),
+    FOREIGN KEY (location_id)
+        REFERENCES location (id),
+    CONSTRAINT FK_company_location
+        UNIQUE (company_id, location_id)
 );
 
-CREATE TABLE review(
-                       id INT NOT NULL AUTO_INCREMENT ,
-                       rate INT NOT NULL,
-                       comment VARCHAR(255),
-                       date DATETIME NOT NULL,
-                       discount_id INT NOT NULL,
-                       employee_id INT NOT NULL,
-                       PRIMARY KEY (id),
-                       FOREIGN KEY (discount_id) REFERENCES discount(id),
-                       FOREIGN KEY (employee_id) REFERENCES employee(id)
+CREATE TABLE discount
+(
+    id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50)  NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    periodStart DATETIME     NOT NULL,
+    periodEnd   DATETIME     NOT NULL,
+    quantity    INT DEFAULT 0,
+    promocode   VARCHAR(50)  NOT NULL,
+    company_id  INT,
+    modified    DATETIME,
+    modifiedBy  VARCHAR(50),
+    foreign key (company_id) references company (id)
+);
+
+CREATE TABLE tag
+(
+    id      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tagName VARCHAR(50)
+);
+
+CREATE TABLE discount_tag
+(
+    id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    discountId INT NOT NULL,
+    tagId      INT NOT NULL,
+    FOREIGN KEY (discountId) REFERENCES discount (id),
+    FOREIGN KEY (tagId) REFERENCES tag (id),
+    CONSTRAINT FK_discount_tag
+        UNIQUE (discountId, tagId)
+);
+
+CREATE TABLE employee_discount
+(
+    id         INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    employeeId INT NOT NULL,
+    discountId INT NOT NULL,
+    FOREIGN KEY (employeeId) REFERENCES employee (id),
+    FOREIGN KEY (discountId) REFERENCES discount (id),
+    CONSTRAINT FK_employee_discount
+        UNIQUE (employeeId, discountId)
+);
+
+CREATE TABLE review
+(
+    id          INT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    rate        INT      NOT NULL,
+    comment     VARCHAR(500),
+    date        DATETIME NOT NULL,
+    discount_id INT      NOT NULL,
+    employee_id INT      NOT NULL,
+    FOREIGN KEY (discount_id) REFERENCES discount (id),
+    FOREIGN KEY (employee_id) REFERENCES employee (id)
 );
