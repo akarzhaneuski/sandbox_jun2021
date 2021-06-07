00767689bea2bd04d027b1952a46252dfbc54ba1
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-08 00:04:32 +0300

    Added migration script with seed data for location.

---
+++ sandbox-backend/src/main/resources/db/migration/V0000002_addLocation.sql
@@ -0,0 +1,6 @@
+INSERT INTO location (city) VALUES ('Lviv');
+INSERT INTO location (city) VALUES ('Kyiv');
+INSERT INTO location (city) VALUES ('Kharkiv');
+INSERT INTO location (city) VALUES ('Minsk');
+INSERT INTO location (city) VALUES ('Grodno');
+INSERT INTO location (city) VALUES ('Brest');
