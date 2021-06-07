7d75036a13929c4a7ebb23a9d53a130ffe7be949
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-07 18:33:46 +0300

    Add migration script with default users

---
+++ sandbox-backend/src/main/resources/db/migration/V0000001_addEmployee.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee VALUES (null, 'E00001', 'pass1', 'USER', 1, null, null);
+INSERT INTO employee VALUES (null, 'E00002', 'pass11', 'USER', 2, null, null);
+INSERT INTO employee VALUES (null, 'E00003', 'pass12', 'USER', 3, null, null);
+INSERT INTO employee VALUES (null, 'E00004', 'pass13', 'USER', 1, null, null);
+INSERT INTO employee VALUES (null, 'M00001', 'pass00', 'MODERATOR', 2, null, null);
