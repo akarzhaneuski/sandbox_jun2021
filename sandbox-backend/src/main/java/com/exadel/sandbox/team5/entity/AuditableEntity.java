c2a655f2a7cfbdb691b5ddce94703e1ca4b675be
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-08 12:52:52 +0300

    refactored annotation

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/AuditableEntity.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/AuditableEntity.java
@@ -1,20 +1,17 @@
 package com.exadel.sandbox.team5.entity;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.util.Date;
 
-@Data
+@Setter
+@Getter
 @MappedSuperclass
-public abstract class AuditableEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @Column(name = "id")
-    private Long id;
+public abstract class AuditableEntity extends BaseEntity {
 
     @Column(name = "modified")
     @LastModifiedDate
