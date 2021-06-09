4d5399749a658ae0f84e7c89ffd1569f38508b54
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-09 23:57:04 +0300

    CompanyDAO has been added.

---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/dao/CompanyDAO.java
@@ -0,0 +1,14 @@
+package com.exadel.sandbox.team5.dao;
+
+import com.exadel.sandbox.team5.entity.Company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompanyDAO extends JpaRepository<Company, Long> {
+
+    List<Company> findAllByLocationsId(long id);
+}
