ccaa6ada300db5d96d310ce2e2f47ac6b9db8d9f
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-09 23:54:56 +0300

    Company entity has been added.

---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/Company.java
@@ -0,0 +1,28 @@
+package com.exadel.sandbox.team5.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "company")
+public class Company extends AuditableEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "companies")
+    Set<Location> locations = new HashSet<>();
+}
