fb2f59e32f4554af958493282f57584e951552ef
Alex <plumbum531@gmail.com>
2021-06-10 22:47:41 +0300

    Remarks about review is implemented.

--- sandbox-web/src/main/java/com/exadel/sandbox/team5/ReviewRestController.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team5/ReviewRestController.java
@@ -4,6 +4,7 @@ import com.exadel.sandbox.team5.entity.DemoEntity;
 import com.exadel.sandbox.team5.entity.Review;
 import com.exadel.sandbox.team5.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -18,15 +19,10 @@ import java.util.stream.Collectors;
 public class ReviewRestController {
     private final ReviewService service;
 
-//    Add endpoint to find reviews for discount (/api/discount/{discountId}/reviews)
-
     @GetMapping("/{id}")
     public List<Review> save(@PathVariable Long id) {
         if (id != null && service.getById(id) != null) {
-            System.out.println("Hello");
-            return service.getAll().stream().
-                    filter(entity -> entity.getDiscount().getId().equals(id)).
-                    collect(Collectors.toList());
+            return service.getAll().stream().filter(entity -> entity.getDiscount().getId().equals(id)).collect(Collectors.toList());
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unknown discount id");
         }
