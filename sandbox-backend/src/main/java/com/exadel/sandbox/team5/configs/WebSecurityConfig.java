43e495fc29f291f3b8ba834504966738350cad38
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-10 21:51:58 +0300

    refactor auth methods

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/configs/WebSecurityConfig.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/configs/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("auth/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
