d90f9e4098d47bae3da29f31669c5d66d3f9aa0f
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-12 14:45:08 +0300

    add discount_tag and add companies in records discount

--- sandbox-backend/src/main/resources/db/migration/V0000006__addDiscount.sql
+++ sandbox-backend/src/main/resources/db/migration/V0000006__addDiscount.sql
@@ -6,3 +6,8 @@ INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
 VALUES ('Cinema', 'Free ticket', '10.05.2021', '15.07.2021','F4U');
 INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
 VALUES ('SportLife', 'Free Month', '10.05.2021', '15.07.2021','BICEPS');
+
+UPDATE discount SET companyId = 3 WHERE id =1;
+UPDATE discount SET companyId = 3 WHERE id =2;
+UPDATE discount SET companyId = 3 WHERE id =3;
+UPDATE discount SET companyId = 5 WHERE id =4;
