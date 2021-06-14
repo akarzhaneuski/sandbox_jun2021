59adf920ca336c19cacd92ffe8341a9b228daa7d
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-14 14:44:59 +0300

    changed data format.

--- sandbox-backend/src/main/resources/db/migration/V0000006__addDiscount.sql
+++ sandbox-backend/src/main/resources/db/migration/V0000006__addDiscount.sql
@@ -1,9 +1,9 @@
 INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
-VALUES ('KING KEBAB', 'Good Kebab', '10.05.2021', '15.07.2021','kebab');
+VALUES ('KING KEBAB', 'Good Kebab', '2021.05.', '2021.07.15','kebab');
 INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
-VALUES ('I <3 KEBAB', 'Best Kebab ever', '10.05.2021', '15.07.2021','kebabkebab');
+VALUES ('I <3 KEBAB', 'Best Kebab ever', '2021.05.10', '2021.07.15','kebabkebab');
 INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
-VALUES ('Cinema', 'Free ticket', '10.05.2021', '15.07.2021','F4U');
+VALUES ('Cinema', 'Free ticket', '2021.05.10', '2021.07.15','F4U');
 INSERT INTO discount (name, description, periodStart, periodEnd, promocode)
-VALUES ('SportLife', 'Free Month', '10.05.2021', '15.07.2021','BICEPS');
+VALUES ('SportLife', 'Free Month', '2021.05.10', '2021.07.15','BICEPS');
 
