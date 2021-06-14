398e98acaef3a9c467cc092768ec869d56eead52
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-14 15:52:02 +0300

    added Logger

--- sandbox-web/src/main/java/com/exadel/sandbox/team5/error/RestExceptionHandler.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team5/error/RestExceptionHandler.java
@@ -1,39 +1,41 @@
 package com.exadel.sandbox.team5.error;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.logging.LogManager;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+    private static Logger logger = Logger.getLogger("RestExceptionHandler");
 
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ResponseBody
     protected ResponseEntity<Object> handleEntityNotFoundEx(Exception ex, WebRequest request) {
+        logger.log(Level.SEVERE, "Cannot find entity by Id", ex);
         ApiError apiError = new ApiError("Entity Not Found Exception ", ex.getMessage());
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Object> handleNoHandlerFoundException(Exception ex, WebRequest request) {
+        logger.log(Level.SEVERE, "Page was not found", ex);
+        ApiError apiError = new ApiError("Page Not Found Exception ", ex.getMessage());
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
                                                                       WebRequest request) {
+        logger.log(Level.SEVERE, "Method argument type mismatch", ex);
         ApiError apiError = new ApiError();
         apiError.setMessage(String.format("The parameter '%s' of value '%s' could not be converted to type '%s'",
                 ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
@@ -43,6 +45,7 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        logger.log(Level.SEVERE, "Internal Exception", ex);
         ApiError apiError = new ApiError("Internal Exception", ex.getMessage());
         return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
