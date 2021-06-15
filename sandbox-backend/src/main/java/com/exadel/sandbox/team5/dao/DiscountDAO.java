36a7442128b4c8e3602365fea602a6651dfa79d6
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-15 14:37:59 +0300

    refactored SearchCriteria abd SearchResult.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/dao/DiscountDAO.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/dao/DiscountDAO.java
@@ -1,10 +1,13 @@
 package com.exadel.sandbox.team5.dao;
 
+import com.exadel.sandbox.team5.dao.util.SearchResult;
 import com.exadel.sandbox.team5.entity.Discount;
+import com.exadel.sandbox.team5.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface DiscountDAO extends JpaRepository<Discount, Long> {
-
 }
