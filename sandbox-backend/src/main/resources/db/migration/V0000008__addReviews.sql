3298632b1ef56f44de81a7db2b27ff4ae317f77c
Alex <plumbum531@gmail.com>
2021-06-16 00:30:37 +0300

    Add entities, DAO's, Services for review and employee_discount tables.
    Add endpoint to find review.
    
    Employee discount relationship is created.
    
    Reviews.sql is added.

---
+++ sandbox-backend/src/main/resources/db/migration/V0000008__addReviews.sql
@@ -0,0 +1,7 @@
+insert into review (rate, comment, date, discountId, employeeId)
+values (5, 'Nice', '25.06.2020', 1, 1),
+       (5, 'Good', '12.08.2020', 1, 2),
+       (4, 'Like good', '10.01.2020', 2, 4),
+       (1, 'Too bad', '5.07.2020', 3, 4),
+       (2, 'Poor service', '3.03.2020', 4, 3),
+       (4, 'Must be better', '30.0612.2020', 4, 3);
