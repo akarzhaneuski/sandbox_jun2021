aa07d8ccee60b19993ef46a761a8982a5a4d28d5
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-06-15 13:14:37 +0300

    fix import

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/Company.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/Company.java
@@ -2,10 +2,7 @@ package com.exadel.sandbox.team5.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import java.util.HashSet;
 import java.util.Set;
