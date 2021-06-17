1283b58b970d0d1b6bebd937be255176d1d860f7
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-18 00:56:59 +0300

    added script

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/dao/ReviewDAO.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/dao/ReviewDAO.java
@@ -2,6 +2,11 @@ package com.exadel.sandbox.team5.dao;
 
 import com.exadel.sandbox.team5.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewDAO extends JpaRepository<Review, Long> {
+
+    @Query(value = "SELECT AVG (r.rate) from Review r where r.id= :id")
+    Double findRate(Long id);
+
 }
