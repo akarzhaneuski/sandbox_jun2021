dc030b2d224a37fc4998ac1da3d503815dbfb102
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-17 17:38:03 +0300

    refactored SearchCriteria

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/SearchCriteria.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/SearchCriteria.java
@@ -1,27 +1,15 @@
 package com.exadel.sandbox.team5.util;
 
-import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-
-import java.util.Date;
-import java.util.List;
 
 public class SearchCriteria {
     private PageRequest pageRequest;
-    /*private Date date;
-    private List<String> properties;
-    private long number;*/
 
     private SearchCriteria() {
     }
 
-    public SearchCriteria(int pageNum, int itemsPerPage, Sort.Direction direction,
-                          Date date, List<String> properties, long number) {
-        properties.add(date.toString());
-        properties.add(""+number);
-        this.pageRequest = PageRequest.of(pageNum, itemsPerPage, direction,
-                String.valueOf(properties));
+    public SearchCriteria(PageRequest pageRequest) {
+        this.pageRequest = pageRequest;
     }
 
     public PageRequest getPageRequest() {
