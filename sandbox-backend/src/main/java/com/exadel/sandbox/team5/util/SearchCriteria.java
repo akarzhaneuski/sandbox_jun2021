2ef4944e122aedd3389b1830aa3bb82fe6e3d6dd
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-17 13:33:49 +0300

    refactored SearchCriteria.

---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/SearchCriteria.java
@@ -0,0 +1,55 @@
+package com.exadel.sandbox.team5.util;
+
+import org.springframework.data.domain.PageRequest;
+
+import java.util.Date;
+import java.util.List;
+
+public class SearchCriteria {
+    private PageRequest pageRequest;
+    private Date date;
+    private List<String> properties;
+    private long number;
+
+    private SearchCriteria() {
+    }
+
+    public SearchCriteria(PageRequest pageRequest, Date date, List<String> properties, long number) {
+        this.pageRequest = pageRequest;
+        this.date = date;
+        this.properties = properties;
+        this.number = number;
+    }
+
+    public SearchCriteria(PageRequest pageRequest, Date date, List<String> properties) {
+        this.pageRequest = pageRequest;
+        this.date = date;
+        this.properties = properties;
+    }
+
+    public SearchCriteria(PageRequest pageRequest, Date date) {
+        this.pageRequest = pageRequest;
+        this.date = date;
+    }
+
+    public SearchCriteria(PageRequest pageRequest) {
+        this.pageRequest = pageRequest;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<String> getProperties() {
+        return properties;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public PageRequest getPageRequest() {
+        return pageRequest;
+    }
+}
+
