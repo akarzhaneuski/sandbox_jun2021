20cb64bc3e9720e17892cb05115085a209e515f9
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-20 14:20:47 +0300

    Created static search and modified SearchCriteria.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/SearchCriteria.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/SearchCriteria.java
@@ -1,15 +1,32 @@
 package com.exadel.sandbox.team5.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @Getter
-@AllArgsConstructor
 public class SearchCriteria {
-    private PageRequest pageRequest;
+    private int pageNum;
+    private int itemsPerPage;
+    private String direction;
+    private String properties;
 
-    private SearchCriteria() {
+    protected SearchCriteria() {
+    }
+
+    public SearchCriteria(int pageNum, int itemsPerPage, String direction, String properties) {
+        this.pageNum = pageNum;
+        this.itemsPerPage = itemsPerPage;
+        this.direction = direction;
+        this.properties = properties;
+    }
+
+    public PageRequest getPageRequest() {
+        Sort.Direction d;
+        if (direction.equals("ASC")) d = Sort.Direction.ASC;
+        else if (direction.equals("DESC")) d = Sort.Direction.DESC;
+        else d = Sort.Direction.fromString(direction);
+        return PageRequest.of(pageNum, itemsPerPage, d, properties);
     }
 }
 
