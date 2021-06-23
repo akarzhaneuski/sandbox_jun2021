438d94d8d19ce86c5abce30da8c173ead2cc456b
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-23 15:32:04 +0300

    added rate to DiscountDTOs.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/DiscountServiceImpl.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/DiscountServiceImpl.java
@@ -2,18 +2,19 @@ package com.exadel.sandbox.team5.service.impl;
 
 import com.exadel.sandbox.team5.dao.DiscountDAO;
 import com.exadel.sandbox.team5.dao.ReviewDAO;
-import com.exadel.sandbox.team5.dao.TagDAO;
 import com.exadel.sandbox.team5.dto.DiscountDto;
 import com.exadel.sandbox.team5.entity.Discount;
 import com.exadel.sandbox.team5.mapper.MapperConverter;
 import com.exadel.sandbox.team5.service.DiscountService;
 import com.exadel.sandbox.team5.util.DiscountSearchCriteria;
+import com.exadel.sandbox.team5.util.RateSetter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -25,7 +26,6 @@ public class DiscountServiceImpl implements DiscountService {
 
     private final DiscountDAO discountDAO;
     private final MapperConverter mapper;
-    private final TagDAO tagDAO;
     private final ReviewDAO reviewDAO;
 
     @Override
@@ -72,18 +72,34 @@ public class DiscountServiceImpl implements DiscountService {
     public Page<DiscountDto> getByCriteria(DiscountSearchCriteria searchCriteria) {
         String searchText = getWildcard(searchCriteria.getSearchText());
         List<Discount> result;
+        List<Long> discountId = new ArrayList<>();
         if (searchCriteria.getTags() == null || searchCriteria.getTags().isEmpty()) {
             result = discountDAO.getByCriteria(searchText, searchCriteria.getRate());
         } else {
             result = discountDAO.getByCriteriaWithTags(searchText,
                     searchCriteria.getTags(), searchCriteria.getRate());
         }
+        result.forEach(x -> discountId.add(x.getId()));
+        List<RateSetter> rateList = reviewDAO.getRateByDiscountId(discountId);
         List<DiscountDto> discountDTOs = mapper.mapAll(result, DiscountDto.class);
+        discountDTOs = setRate(rateList, discountDTOs);
         return new PageImpl<>(discountDTOs, searchCriteria.getPageRequest(), discountDTOs.size());
     }
 
     private String getWildcard(String text) {
         return "%" + text + "%";
+    }
+
+    private List<DiscountDto> setRate(List<RateSetter> rtList, List<DiscountDto> dtoList) {
+        for (DiscountDto d : dtoList) {
+            for (RateSetter r : rtList) {
+                if (d.getId() == r.getDiscountId()) {
+                    d.setRate((double) r.getRate());
+                    break;
+                }
+            }
+        }
+        return dtoList;
     }
 }
 
---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/util/RateSetter.java
@@ -0,0 +1,13 @@
+package com.exadel.sandbox.team5.util;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class RateSetter {
+    private int rate = 0;
+    private long discountId = 0;
+}
