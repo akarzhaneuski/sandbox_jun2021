ac82ffd5f313decd82454267616057f8f6c182f7
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-25 01:11:43 +0300

    Deleted unused import Log4j2

--- sandbox-web/src/main/java/com/exadel/sandbox/team5/controller/DiscountRestController.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team5/controller/DiscountRestController.java
@@ -4,10 +4,8 @@ import com.exadel.sandbox.team5.dto.DiscountDto;
 import com.exadel.sandbox.team5.entity.Review;
 import com.exadel.sandbox.team5.service.DiscountService;
 import com.exadel.sandbox.team5.service.ReviewService;
-import com.exadel.sandbox.team5.service.impl.DiscountServiceImpl;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +16,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/discounts")
 @RequiredArgsConstructor
-@Log4j2
 public class DiscountRestController {
 
     private final DiscountService service;
     private final ReviewService reviewService;
-    private final DiscountServiceImpl discountService;
+    private final DiscountService discountService;
 
     @GetMapping("/{id}")
     public DiscountDto getDiscount(@PathVariable Long id) {
