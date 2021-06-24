f306c52ba3c5f15f3bff5db6bcc6fab64ae3398a
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-25 01:15:38 +0300

    Created method for qr code generating.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/DiscountService.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/DiscountService.java
@@ -3,4 +3,10 @@ package com.exadel.sandbox.team5.service;
 import com.exadel.sandbox.team5.dto.DiscountDto;
 
 public interface DiscountService extends CRUDService<DiscountDto> {
+
+    /**
+     * Generate QR code.
+     * @return byte array as QR code image representation.
+     */
+    byte[] generateQRCode();
 }
