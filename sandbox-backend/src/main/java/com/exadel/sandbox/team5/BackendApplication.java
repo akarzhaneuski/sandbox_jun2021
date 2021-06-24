48bb3c272e423f6031fb1e2a9a1d72b57b9c18a3
mgoroshko <68497558+MaryGoroshko@users.noreply.github.com>
2021-06-25 01:24:59 +0300

    Created test for QR code.

---
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/BackendApplication.java
@@ -0,0 +1,20 @@
+package com.exadel.sandbox.team5;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+@ComponentScan
+@EnableAutoConfiguration
+public class BackendApplication {
+
+    @Bean
+    public ModelMapper getMapper() {
+        return new ModelMapper();
+    }
+}
--- sandbox-web/src/main/java/com/exadel/sandbox/team5/Application.java
+++ sandbox-web/src/main/java/com/exadel/sandbox/team5/Application.java
@@ -1,20 +1,14 @@
 package com.exadel.sandbox.team5;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(BackendApplication.class)
 public class Application {
-
-	@Bean
-	public ModelMapper getMapper() {
-		return new ModelMapper();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
