0c37247a6c883e9ef065c5f0ebf05a5fee0536a6
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-06-25 20:44:06 +0300

    added statistic by companyId and tags.

---
+++ sandbox-backend/src/main/resources/db/migration/V0000015__setCompany_to_Discount.sql
@@ -0,0 +1,4 @@
+UPDATE discount d SET companyId=3 WHERE d.id=1;
+UPDATE discount d SET companyId=3 WHERE d.id=2;
+UPDATE discount d SET companyId=1 WHERE d.id=3;
+UPDATE discount d SET companyId=5 WHERE d.id=4;
