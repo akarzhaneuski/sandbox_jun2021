e0ae77979fd78c6acb0efed6d07ef283cf057764
Alex <plumbum531@gmail.com>
2021-07-06 08:50:02 +0300

    Common CRUD for Company, Employee and Tag is created.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/CRUDServiceImpl.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/CRUDServiceImpl.java
@@ -6,6 +6,7 @@ import com.exadel.sandbox.team5.service.CRUDService;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 class CRUDServiceImpl<E extends BaseEntity, S extends CommonRepository<E>> implements CRUDService<E> {
@@ -13,19 +14,19 @@ class CRUDServiceImpl<E extends BaseEntity, S extends CommonRepository<E>> imple
     protected final S repository;
 
     public E getById(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
     public List<E> getAll() {
         return repository.findAll();
     }
 
-    public E save(E discount) {
-        return repository.saveAndFlush(discount);
+    public E save(E entity) {
+        return repository.saveAndFlush(entity);
     }
 
-    public E update(E discount) {
-        return repository.save(discount);
+    public E update(E entity) {
+        return repository.save(entity);
     }
 
     public void delete(Long id) {
--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/CompanyServiceImpl.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/CompanyServiceImpl.java
@@ -1,54 +1,24 @@
 package com.exadel.sandbox.team5.service.impl;
 
-import com.exadel.sandbox.team5.dao.CommonRepository;
 import com.exadel.sandbox.team5.dao.CompanyDAO;
 import com.exadel.sandbox.team5.entity.Company;
 import com.exadel.sandbox.team5.service.CompanyService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Transactional
 @Service
-//@RequiredArgsConstructor
-public class CompanyServiceImpl extends CRUDServiceImpl implements CompanyService {
+public class CompanyServiceImpl extends CRUDServiceImpl <Company, CompanyDAO> implements CompanyService {
 
-    private final CompanyDAO dao;
 
-    public CompanyServiceImpl(CommonRepository repository) {
+    public CompanyServiceImpl(CompanyDAO repository) {
         super(repository);
     }
 
     @Override
-    public Company getById(Long id) {
-        return dao.findById(id).orElseThrow(NoSuchElementException::new);
-    }
-
-    @Override
-    public List<Company> getAll() {
-        return dao.findAll();
-    }
-
-    @Override
-    public Company save(Company company) {
-        return dao.save(company);
-    }
-
-    @Override
-    public Company update(Company company) {
-        return dao.save(company);
-    }
-
-    @Override
-    public void delete(Long id) {
-        dao.deleteById(id);
-    }
-
-    @Override
     public List<Company> getCompaniesByLocation(Long id) {
-        return dao.findAllByCountryId(id);
+        return repository.findAllByCountryId(id);
     }
 }
