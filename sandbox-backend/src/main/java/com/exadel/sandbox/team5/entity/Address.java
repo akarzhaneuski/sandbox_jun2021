d3ccd350c95c0e91a46fffa6069622a1c16c5a9a
Yevhen Dobrenchuk <lukeskywolker07@gmail.com>
2021-07-12 16:32:27 +0300

    added cascade type persist

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/Address.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/entity/Address.java
@@ -3,6 +3,7 @@ package com.exadel.sandbox.team5.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,7 +14,7 @@ import java.util.Set;
 @Builder
 @Entity
 @Table(name = "address")
-public class Address extends BaseEntity {
+public class Address extends BaseEntity implements Serializable {
 
     @Column(name = "address")
     private String address;
