462d2e70effa8891888cf7b2472f3ea41b0b0c46
Alex <plumbum531@gmail.com>
2021-07-23 21:27:52 +0300

    add Unique for name: discount, tag, country, city, category.

---
+++ sandbox-backend/src/main/resources/db/migration/V0000032__addUnique.sql
@@ -0,0 +1,5 @@
+ALTER TABLE discount ADD UNIQUE (name);
+ALTER TABLE tag ADD UNIQUE (tagName);
+ALTER TABLE country ADD UNIQUE (name);
+ALTER TABLE city ADD UNIQUE (name);
+ALTER TABLE category ADD UNIQUE (name);
