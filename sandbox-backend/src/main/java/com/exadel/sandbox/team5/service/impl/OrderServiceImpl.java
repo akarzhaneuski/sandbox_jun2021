e0701a1a9617bad92d82d64e133a56f582fbe465
Alex <plumbum531@gmail.com>
2021-07-27 10:26:34 +0300

    Refactor.

--- sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/OrderServiceImpl.java
+++ sandbox-backend/src/main/java/com/exadel/sandbox/team5/service/impl/OrderServiceImpl.java
@@ -25,7 +25,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
--- sandbox-backend/src/test/java/com/exadel/sandbox/team5/service/impl/OrderServiceImplTest.java
+++ sandbox-backend/src/test/java/com/exadel/sandbox/team5/service/impl/OrderServiceImplTest.java
@@ -6,43 +6,54 @@ import com.exadel.sandbox.team5.dao.OrderDAO;
 import com.exadel.sandbox.team5.dao.ReviewDAO;
 import com.exadel.sandbox.team5.dto.DiscountDto;
 import com.exadel.sandbox.team5.entity.Discount;
+import com.exadel.sandbox.team5.entity.Employee;
 import com.exadel.sandbox.team5.entity.Image;
+import com.exadel.sandbox.team5.entity.Order;
 import com.exadel.sandbox.team5.mapper.MapperConverter;
 import com.exadel.sandbox.team5.service.DiscountService;
 import com.exadel.sandbox.team5.service.EmployeeService;
 import com.exadel.sandbox.team5.service.OrderService;
 import com.exadel.sandbox.team5.util.SecurityUtils;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 class OrderServiceImplTest {
 
+    static final String amountDiscountDays = "7";
 
     @Mock
     EmployeeService employeeService;
 
-    @Spy
+    @Mock
     CompanyDAO companyDAO;
 
-    @Spy
+    @Mock
     DiscountDto discountDto;
 
     @Mock
     MapperConverter mapperConverter;
 
-    @Spy
+    @Mock
     Discount discount;
 
     @Mock
@@ -57,15 +68,11 @@ class OrderServiceImplTest {
     @Mock
     DiscountDAO discountDAO;
 
-    @Spy
+    @Mock
     SecurityUtils securityUtils;
 
-    @Spy
+    @Mock
     OrderDAO orderDAO;
-
-//    @InjectMocks
-//    OrderServiceImpl orderService = new OrderServiceImpl(orderDAO, mapperConverter, employeeService, discountService, discountDAO, companyDAO, securityUtils);
-
 
     @Mock
     OrderService orderSer;
@@ -73,53 +80,70 @@ class OrderServiceImplTest {
     @InjectMocks
     OrderServiceImpl orderService;
 
-    @Before
+    @BeforeEach
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
+//    @BeforeEach
+//    void init() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+
+    @Test
+    public void testInvalidatePromoCode(){
+        Order selectedOrder = new Order();
+        var now = LocalDateTime.now();
+        var periodStart = Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+        var periodEnd = Date.from((now.plusDays(Long.parseLong(amountDiscountDays))).atZone(ZoneId.systemDefault()).toInstant());
+        selectedOrder.setPromoCodeStatus(true);
+        selectedOrder.setPromoCodePeriodStart(periodStart);
+        selectedOrder.setPromoCodePeriodEnd(periodEnd);
+        when(orderService.entityDao.getOrderByEmployeePromocode("87f532ab-3ded-4930-aa5d-02107f78ae9c")).
+                thenReturn(selectedOrder);
+        do(selectedOrder.setP;).when(orderService.entityDao.setPromoCodeStatus(false, "87f532ab-3ded-4930-aa5d-02107f78ae9c")).
+        assertEquals("false", selectedOrder.getPromoCodeStatus());
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    void testCreateEmployeePromoCodeException() throws IllegalArgumentException {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Discount not found");
+        when(discountService.getById(Mockito.anyLong())).thenReturn(null);
     }
 
     @Test
-    void testCreateEmployeePromoCode() throws NoSuchElementException {
-        Mockito.when(discountService.getById(1L)).thenReturn(new DiscountDto());
-        Mockito.when(discountService.getById(1L)).thenReturn(new DiscountDto());
-        assertEquals(String.class, orderService.createOrder("3"));
-
+    void testCreateEmployeePromoCode() {
+        when(discountService.getById(Mockito.anyLong())).thenReturn(new DiscountDto());
+        mockStatic(SecurityUtils.class);
+        when(SecurityUtils.getCurrentUsername()).thenReturn("E00001");
+        when(employeeService.getByLogin(SecurityUtils.getCurrentUsername())).thenReturn(new Employee());
+        orderService.amountDiscountDays = "7";
+        assertEquals(String.class, orderService.createOrder("3").getClass());
     }
 
     @Test
     void getOrdersByDiscountsTest() {
         Map<String, String> testMap = orderService.getOrdersByDiscounts();
         assertNotNull(testMap);
-        assertEquals(new HashMap().getClass(), testMap.getClass());
+        assertEquals(HashMap.class, testMap.getClass());
     }
 
     @Test
     void getOrdersByCompaniesTest() {
-        Map<String, String> testMap = orderService.getOrdersByCompanies();
-        assertNotNull(testMap);
-        assertEquals(new HashMap().getClass(), testMap.getClass());
+        assertEquals(HashMap.class, orderService.getOrdersByCompanies().getClass());
     }
 
     @Test
     void getOrdersByTagsTest() {
-        Map<String, String> testMap = orderService.getOrdersByTags();
-        assertNotNull(testMap);
-        assertEquals(new HashMap().getClass(), testMap.getClass());
+        assertEquals(HashMap.class, orderService.getOrdersByTags().getClass());
     }
 
     @Test
     void getOrdersByCategoriesTest() {
-        Map<String, String> testMap = orderService.getOrdersByCategories();
-        assertNotNull(testMap);
-        assertEquals(new HashMap().getClass(), testMap.getClass());
-    }
-
-    @Test
-    void createOrder() {
+        assertEquals(HashMap.class, orderService.getOrdersByCategories().getClass());
     }
 }
