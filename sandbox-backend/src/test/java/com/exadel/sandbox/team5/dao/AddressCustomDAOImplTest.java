36fdd36387d23c5d9e42b7f9a335c149e94640ed
anatolii-tykhonko <anatolii.tkhnk@gmail.com>
2021-07-28 17:48:16 +0300

    create tests on AddressService

---
+++ sandbox-backend/src/test/java/com/exadel/sandbox/team5/dao/AddressCustomDAOImplTest.java
@@ -0,0 +1,12 @@
+package com.exadel.sandbox.team5.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressCustomDAOImplTest {
+
+    @Test
+    void saveOrUpdate() {
+    }
+}
